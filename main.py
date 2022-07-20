ba44964bbac8528935161eaa03cbd75b43ba821e
cucch <cucch@DESKTOP-5K82ICH>
2022-07-20 19:15:56 +0200

    Plot loss data and made the model purely linear
    Also moved the code that print and saves the model outside the training
    loop

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import os.path
 import hotEncoding as hE
+import pandas as pd
+import matplotlib.pyplot as plt
 
-N_EPOCHS = 120
-LR = 5e-4
+N_EPOCHS = 20000
+LR = 1e-3
 
 
 class Model(nn.Module):
     def __init__(self, numInFeatures):
         super(Model, self).__init__()
-        self.hidden_layer = nn.Linear(numInFeatures, 17)
-        self.hidden_activation = nn.Tanh()
-        self.output_linear = nn.Linear(17, 1)
+        self.hidden_layer = nn.Linear(numInFeatures, 1)
+        #self.hidden_activation = nn.Tanh()
+        self.output_linear = nn.Linear(1, 1)
         torch.nn.init.normal_(self.output_linear.weight, mean=0, std=1e-9)
 
     def forward(self, input):
         hidden_t = self.hidden_layer(input)
-        activated_t = self.hidden_activation(hidden_t)
-        output_t = self.output_linear(activated_t)
+        #activated_t = self.hidden_activation(hidden_t)
+        #output_t = self.output_linear(activated_t)
+        output_t = self.output_linear(hidden_t)
         return output_t
 
 
@@ -39,19 +41,34 @@ def main():
     t_inp_test = testTensor[:, 0: trainTensor.shape[1]-1]
 
     t_res_train = trainTensor[:, trainTensor.shape[1]-1]
-    t_res_val = testTensor[:, trainTensor.shape[1]-1]
+    t_res_test = testTensor[:, trainTensor.shape[1]-1]
     t_res_train = t_res_train.unsqueeze(1)
-    t_res_val = t_res_val.unsqueeze(1)
-
-    print(t_inp_train, t_inp_train.shape)
-    print(t_inp_test, t_inp_test.shape)
-    print(t_res_train, t_res_train.shape)
-    print(t_res_val, t_res_val.shape)
+    t_res_test = t_res_test.unsqueeze(1)
 
     model = Model(t_inp_train.shape[1])
     optimizer = optim.Adam(model.parameters(), lr=LR)
     trainingLoop(n_epochs=N_EPOCHS, optimizer=optimizer, model=model, loss_fn=nn.MSELoss(
-    ), t_inp_train=t_inp_train, t_inp_val=t_inp_test, t_res_val=t_res_val, t_res_train=t_res_train)
+    ), t_inp_train=t_inp_train, t_inp_test=t_inp_test,  t_res_train=t_res_train, t_res_test=t_res_test)
+
+    torch.save(model.state_dict(), 'model/model.pth')
+
+    print('ouput', model(t_inp_test))
+
+    outputTensor = model(t_inp_test)
+
+    for i in range(outputTensor.shape[0]):
+        if outputTensor[i] <= 0.5:
+            outputTensor[i] = 0
+        else:
+            outputTensor[i] = 1
+
+    difference = outputTensor-t_res_test
+    print(difference, difference.shape)
+    difference = torch.abs(difference)
+
+    print("ratio errors: " + str(torch.sum(difference)/difference.shape[0]))
+    print('answer', t_res_test)
+    # print('hidden', model.hidden_layer.weight.grad)
 
 
 def loadData(nameNpyFile, nameFileData):
@@ -77,7 +94,7 @@ def normalizeTensor(adultData):
     n_channels = adultData.shape[1]
     for i in range(0, n_channels-1):
         # -1 because we don't want to normalize the resulta that is already 0 or 1
-        
+
         max = torch.max(adultData[:, i])
         if max > 1e-8:
             min = torch.min(adultData[:, i])
@@ -116,28 +133,40 @@ def mapWords(adultData):
     return adultData
 
 
-def trainingLoop(n_epochs, optimizer, model, loss_fn, t_inp_train, t_inp_val, t_res_train, t_res_val):
+def trainingLoop(n_epochs, optimizer, model, loss_fn, t_inp_train, t_inp_test, t_res_train, t_res_test):
+    results = np.array([["Epoch", "Training Loss", "Test Loss"]])
     for epoch in range(1, n_epochs+1):
         t_p_train = model(t_inp_train)
         loss_train = loss_fn(t_p_train, t_res_train)
 
-        t_p_val = model(t_inp_val)
-        loss_val = loss_fn(t_p_val, t_res_val)
+        t_p_val = model(t_inp_test)
+        loss_val = loss_fn(t_p_val, t_res_test)
 
         optimizer.zero_grad()
         loss_train.backward()
         optimizer.step()
+        results = np.append(
+            results, [[epoch, float(loss_train), float(loss_val)]], axis=0)
 
-        if epoch == 1 or epoch % 10 == 0:
+        if epoch == 1 or epoch % 100 == 0:
             print("Epoch {}, Training loss {}, Validation loss {},".format(
-              epoch, float(loss_train), float(loss_val)))
+                epoch, float(loss_train), float(loss_val)))
 
-    torch.save(model.state_dict(), 'model/model.pth')
-    
-    #TODO: make the output either 0 or 1 and confront
-    print('ouput', model(t_inp_train))
-    print('answer', t_res_train)
-    # print('hidden', model.hidden_layer.weight.grad)
+    printTrainingResults(results, n_epochs)
+
+
+def printTrainingResults(results, n_epochs):
+    # n_epochs+1 because there are the names of the columns in the first row
+    results.reshape(n_epochs+1, 3)
+    index = results[1:, 0]
+    data = results[1:, 1:]
+    columns = results[0, 1:]
+    resultDF = pd.DataFrame(
+        data=data, index=index, columns=columns)
+    resultDF['Training Loss'] = resultDF['Training Loss'].astype(float)
+    resultDF['Test Loss'] = resultDF['Test Loss'].astype(float)
+    resultDF.plot()
+    plt.show()
 
 
 main()
